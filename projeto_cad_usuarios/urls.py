43eeea50a86705728984da4a063ee5922bccdf40
jcharlescm <jcharlescm@gmail.com>
2024-11-28 17:01:00 -0300

    Alteraçao de Mundança de Senha - Change

--- projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from app_cad_usuarios import views
 
@@ -11,6 +12,12 @@ urlpatterns = [
     path('usuarios/usuarios.html', views.lista_usuarios, name='lista_usuarios'),    
     path('usuarios', views.usuarios, name='listagem_usuarios'),
     # http://seu_dominio/assinatura_email, função ou classe view , Dá um nome à URL
-    path('emails', views.inclui_emails, name='inclui_email'),   
+    path('emails', views.inclui_emails, name='inclui_email'),  
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(success_url='password_change_done'),
+        name='password_change'
+        # template_name = "registration/password_change_form.html"
+    ),  
      
 ]
